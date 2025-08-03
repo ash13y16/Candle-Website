ec92db03e2186bfe21579a6908309580a46aa8cc
ash13y16 <ashley20854@gmail.com>
2025-08-03 00:45:42 -0400

    Saving

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import './styles.css';
 
-function ContactPage() {
-    return (
-        <div className="ContactPage">
-
-        </div>
-    );
+export default function ContactPage() {
+    // const [formData, setFormData] = useState({ name: '', product: '', quantity: '' });
+    //
+    // function handleChange(e) {
+    //     setFormData({ ...formData, [e.target.name]: e.target.value });
+    // }
+    //
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     // TODO: send formData to your backend or service
+    //     console.log(formData);
+    // }
+    //
+    // return (
+    //     <form onSubmit={handleSubmit}>
+    //         <input name="name" placeholder="Your Name" onChange={handleChange} />
+    //         <input name="product" placeholder="Product Name" onChange={handleChange} />
+    //         <input name="quantity" placeholder="Quantity" onChange={handleChange} />
+    //         <button type="submit">Submit Order</button>
+    //     </form>
+    // );
 }
-
-export default ContactPage;
