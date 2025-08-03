c616c6f251600ca8c9cc0db7a2067003b008ca30
ash13y16 <ashley20854@gmail.com>
2025-08-03 17:38:31 -0400

    Added steps 1-3

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -2,6 +2,28 @@ import { useState } from "react";
 import './styles.css';
 
 export default function ContactPage() {
+    return (
+        <div className={'ContactPage'}>
+            <div className={'stepContCont'}>
+                <div className={'stepContainer'}>
+                    <h1>1</h1><h2>CHOOSE</h2>
+                </div>
+                <p>This is a description of step 1</p>
+
+                <div className={'stepContainer'}>
+                    <h1>2</h1><h2>ORDER</h2>
+                </div>
+                <p>Step two</p>
+
+                <div className={'stepContainer'}>
+                    <h1>3</h1><h2>PAY & ENJOY</h2>
+                </div>
+                <p style={{height: 'fit-content', borderLeft: "2px solid white"}}>Step three</p>
+            </div>
+
+        </div>
+    );
+
     // const [formData, setFormData] = useState({ name: '', product: '', quantity: '' });
     //
     // function handleChange(e) {
